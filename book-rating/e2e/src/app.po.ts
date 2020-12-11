39a784a3789391ec9b75b75c0310cb5ade08edc8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-12-11 09:25:53 +0100

    fixing e2e test

--- book-rating/e2e/src/app.po.ts
+++ book-rating/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   async getTitleText(): Promise<string> {
-    return element(by.css('br-root .content span')).getText();
+    return element(by.css('h1')).getText();
   }
 }
