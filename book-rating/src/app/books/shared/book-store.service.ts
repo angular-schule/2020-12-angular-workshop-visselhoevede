77e0e6219eda21d95a9454072d072ff04576766a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-12-14 15:29:47 +0100

    rxjs: no error handling! 😳

--- book-rating/src/app/books/shared/book-store.service.ts
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Book } from './book';
 
 @Injectable({
@@ -15,6 +16,8 @@ export class BookStoreService {
   }
 
   getSingleBook(isbn: string): Observable<Book> {
-    return this.http.get<Book>('https://api.angular.schule/book/' + isbn);
+    return this.http.get<Book>('https://api.angular.schule/book/' + isbn).pipe(
+      // delay(Math.floor(Math.random() * 10) + 1) // debug slow connections
+    );
   }
 }
